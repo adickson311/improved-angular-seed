63ea61b06b9fe42eb92d141e43e4d30fdaf1d4fd
John Austin <john.austin1@gmail.com>
2017-05-06 10:02:50 -0400

    Upgrade angular to 1.5 ; upgrade angular ui bootstrap ; add browser sync as npm run dev

--- app/app.controller.js
+++ app/app.controller.js
@@ -2,13 +2,50 @@
     angular.module('app')
         .controller('MyController', MyController);
 
-        MyController.$inject = ['$scope'];
+        MyController.$inject = ['$scope', '$uibModal', '$log'];
 
-        function MyController($scope) {
+        function MyController($scope, $uibModal, $log) {
             var vm = this;
 
-            vm.title = 'My Controller';
+            vm.title = 'My test';
 
-            console.log('MyController');
+            // Instantiate the modal window
+            var modalPopup = function () {
+              return $scope.modalInstance = $uibModal.open({
+                templateUrl: 'modal.html',
+                scope: $scope
+              });
+            };
+
+            // Modal window popup trigger
+            vm.openModalPopup = function () {
+              modalPopup().result
+                .then(function (data) {
+                  $scope.handleSuccess(data);
+                })
+                .then(null, function (reason) {
+                  $scope.handleDismiss(reason);
+                });
+            };
+
+            // Close the modal if Yes button click
+            $scope.yes = function () {
+              $scope.modalInstance.close('Yes Button Clicked')
+            };
+
+            // Dismiss the modal if No button click
+            $scope.no = function () {
+              $scope.modalInstance.dismiss('No Button Clicked')
+            };
+
+            // Log Success message
+            $scope.handleSuccess = function (data) {
+              $log.info('Modal closed: ' + data);
+            };
+
+            // Log Dismiss message
+            $scope.handleDismiss = function (reason) {
+              $log.info('Modal dismissed: ' + reason);
+            }
         }
 })();
